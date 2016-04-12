00daec007dbdd662f3c80fa9d7a90ada6ecbc4f1
Emmanuelraj <immanuel.raj18@gmail.com>
2016-04-12 17:50:14 +0530

    ProHunter28.java

---
+++ ProHunter28.java
@@ -0,0 +1,41 @@
+package com.guvi.codekata.hunter;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/*
+ * given a string remove duplicates from it
+ * hello-->helo
+ * HeLl0-->helo 
+ */
+public class ProHunter28 
+{
+	
+	
+	
+	public static void main(String[] args)
+	{
+	System.out.println("enter the String");
+	Scanner scan=new Scanner(System.in);
+	String str=scan.nextLine();
+	String s1=str.toLowerCase();
+	if(s1!=null||s1.trim().equals(" "))
+	{
+	Set<String>ls=new LinkedHashSet<String>();
+	char[]c=s1.toCharArray();
+	for(char c1:c)
+	{
+		String temp=""+c1;
+		ls.add(temp);
+	}String out="";
+	for(String s:ls)
+	{
+		out=out+s;
+	}
+	System.out.println(out);
+	}else
+	System.out.println("invalid");
+	}
+
+}
